21c2715b75061e1333bf4efe5191b84b58b71eed
bruno_costa_fig <brunocosta.fig@gmail.com>
2025-02-04 21:32:55 -0300

    feat: adição de endpoint para listar e criar roles

--- src/routes/rbac.routes.ts
+++ src/routes/rbac.routes.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router } from "express";
-import { Permission } from "../entity/Permission";
 import { AppDataSource } from "../data-source";
+
+import { Permission } from "../entity/Permission";
+import { Role } from "../entity/Role";
 
 const rbacRouter = Router()
 
 const permissionRepository = AppDataSource.getRepository(Permission)
+const roleRepository = AppDataSource.getRepository(Role)
 
 rbacRouter.get("/listPermissions", async (req: Request, res: Response) => {
     try {
@@ -25,9 +28,37 @@ rbacRouter.post("/createOnePermission", async (req: Request, res: Response) => {
             return
         }
 
-        permissionRepository.save(permissionBody)
+        await permissionRepository.save(permissionBody)
 
         res.status(201).json(permissionBody)
+
+    } catch (ex){
+        res.status(500).json("Erro ao processar solicitação")
+    }
+})
+
+rbacRouter.get("/listRoles", async (req: Request, res: Response) => {
+    try {
+        const roles = await roleRepository.find()
+
+        res.status(200).json(roles)
+    } catch (ex){
+        res.status(500).json("Erro ao processar solicitação")
+    }
+})
+
+rbacRouter.post("/createOneRole", async (req: Request, res: Response) => {
+    try {
+        const roleBody = req.body as Role
+
+        if(!roleBody.description){
+            res.status(400).json("A descrição é obrigatória!")
+            return
+        }
+
+        await roleRepository.save(roleBody)
+
+        res.status(201).json(roleBody)
 
     } catch (ex){
         res.status(500).json("Erro ao processar solicitação")
